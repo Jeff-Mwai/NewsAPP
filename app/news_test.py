28750cbbc294346ebe5b76521f992e8c1a9f3d83
Jeff-Mwai <jeffmwai3@gmail.com>
2020-10-17 13:51:57 +0300

    Added a new test instance

--- app/news_test.py
+++ app/news_test.py
@@ -2,3 +2,21 @@ import unittest
 from models import news
 News = news.News
 
+class NewsTest(unittest.TestCase):
+
+    '''
+    test instance to check the behavior of the news class
+    '''
+    def setUp(self):
+
+        '''
+        This method runs before every test
+        '''
+        self.news1 = News("New York Times", "Trump is a genius", "I don't have DNA, I have USA", "https://trump.com", "https://trump.com", "12th October 2020" )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.news1,News))
+
+if __name__ == '__main__':
+    unittest.main()
+    
